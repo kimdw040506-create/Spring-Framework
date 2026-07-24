fe0d468dbfdfcf01501d9f3ce13ac293a817f11e
kimvvl1 <kimdw040506@gmail.com>
2026-07-25 03:01:17 +0600

    :)

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -3,11 +3,10 @@ package app.model;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component("cat")
-public class Cat extends Animal {
-    @Override
-    public String toString() {
-        return "Im a Cat";
+@Component("dog")
+public class Dog extends Animal{
+     @Override
+    public String toString(){
+         return "I am dog";
     }
 }
-
