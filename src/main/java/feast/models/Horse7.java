c7c86108ebc9ab8d32966ef0ba420442b22a059b
kimvvl1 <kimdw040506@gmail.com>
2026-07-27 19:06:19 +0600

    Combined all the menus

--- src/main/java/feast/models/Horse7.java
+++ src/main/java/feast/models/Horse7.java
@@ -1,8 +1,16 @@
 package feast.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Horse7 {
+
+    @Autowired
+    public Camel8 camel8;
+
     @Override
     public String toString() {
-        return "which is stuffed inside a camel, ";
+        return "which is stuffed inside a camel, " + camel8.toString();
     }
 }
--- src/main/java/feast/models/Lamb5.java
+++ src/main/java/feast/models/Lamb5.java
@@ -1,6 +1,14 @@
 package feast.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Lamb5 {
+
+    private  Goat6 goat6;
+
+    public Lamb5(Goat6 goat6){
+        this.goat6 = goat6;
+    }
 
     @Override
     public String toString() {
